b5bdd07cd7aca8e05b8e386b59818d94b5b27474
Daligz <oscardali370@gmail.com>
2022-05-22 10:29:22 -0500

    check length fix

--- KNN-with-measurements.R
+++ KNN-with-measurements.R
@@ -1,9 +1,14 @@
+check_length = function(a, b) {
+  res = (length(a) == length(b))
+  if (!(res)) stop('Los vectores no tienen la misma longitud!')
+  return(res)
+}
+
+
 euclidean_distance = function(a, b) {
-  if (length(a) == length(b)) {
-    sqrt(sum((a - b)^2))
-  } else {
-    stop('Los vectores no tienen la misma longitud!')
-  }
+  check_length(a, b)
+  sqrt(sum((a - b)^2))
 }
 
 euclidean_distance(1:10, 11:20)
+
