1408e4dee5b37ed7d49e111f0f020a5fcae8d1e8
Daligz <oscardali370@gmail.com>
2022-05-22 10:31:37 -0500

    manhattan distance added

--- KNN-with-measurements.R
+++ KNN-with-measurements.R
@@ -12,3 +12,9 @@ euclidean_distance = function(a, b) {
 
 euclidean_distance(1:10, 11:20)
 
+manhattan_distance = function(a, b) {
+  check_length(a, b)
+  sum(abs(a - b))
+}
+
+euclidean_distance(1:10, 11:20)
