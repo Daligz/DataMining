90e79f065ad9939e068a900c6bc0f6e1b3bdef44
Daligz <oscardali370@gmail.com>
2022-06-02 22:43:01 -0500

    Datasets added and route implemented

--- SOM.R
+++ SOM.R
@@ -2,3 +2,15 @@ library(kohonen)
 library(dplyr)
 library(plot3D)
 library(plot3Drgl)
+
+df <- read.csv('C:\\workspace\\UPP\\Mineria de Datos\\datasets\\HTRU_2.csv')
+class <- df$X0 # Guardo el clasificador
+n_originales <- colnames(df) # Guardamos nombres de columnas 
+df <- select(df, -X0) # Eliminamos el clasificador
+n_nuevos <- c('V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8') # Asignamos nuevos
+colnames(df) <- n_nuevos # Cambiamos nombres
+rm(n_nuevos)
+
+set.seed(100)
+df <- na.omit(df) 
+Z <- scale(df,center=T,scale=T) # Estandarización
