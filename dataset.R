d8d35434291a3b4de23e882c1cb31b2318af29c7
Daligz <oscardali370@gmail.com>
2022-07-14 17:53:59 -0500

    Update names and translated

--- dataset.R
+++ dataset.R
@@ -1,28 +1,28 @@
 pre_data <- data.frame(
   key = 1:10,
-  name = c("Alan", "Axel", "Diego", "Arely", "Saul", "Giselle", "Oscar", "Angel", "Yoana", "Arlette"),
-  value = c(10, 20, 50, 120, 125, 175, 210, 350, 12, 129),
-  month = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
-  year = c(2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022)
+  nombre = c("Lapiz", "Borrador", "Licuado", "Jarra", "Telefono", "Lapicero", "Libreta", "Colores", "Eco egg holder", "Television"),
+  valor = c(10, 20, 50, 120, 125, 175, 210, 350, 12, 129),
+  mes = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+  anio = c(2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022)
 )
 
 gen_data <- function(no_of_recs) {
   
-  name = sample(pre_data$name, no_of_recs, replace = T)
-  value = sample(pre_data$value, no_of_recs, replace = T)
-  month = sample(pre_data$month, no_of_recs, replace = T)
-  year = sample(pre_data$year, no_of_recs, replace = T)
+  nombre = sample(pre_data$nombre, no_of_recs, replace = T)
+  valor = sample(pre_data$valor, no_of_recs, replace = T)
+  mes = sample(pre_data$mes, no_of_recs, replace = T)
+  anio = sample(pre_data$anio, no_of_recs, replace = T)
   
-  dataValues <- data.frame(
-    name = name,
-    value = value,
-    month = month,
-    year = year
+  datavalors <- data.frame(
+    nombre = nombre,
+    valor = valor,
+    mes = mes,
+    anio = anio
   )
   
-  return(dataValues)
+  return(datavalors)
 }
 
-gen_data_values <- gen_data(500)
+gen_data_valors <- gen_data(500)
 
-head(gen_data_values)
+head(gen_data_valors)
